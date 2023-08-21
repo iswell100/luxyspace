a5692b6f0ef1176f0065f6f3800724c7a157303f
iswell100 <welliskandar100@gmail.com>
2023-08-21 13:08:42 +0700

    setting custom theme tailwind config

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./assets/css/app.css";
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline'>Ini text</h1>
+      <h1 className='text-3xl font-bold underline bg-blue-100'>Ini text</h1>
     </div>
   );
 }
