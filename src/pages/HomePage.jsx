dfe76c613dc3dfa89dfc5fcd33ff610147e5b2f7
iswell100 <welliskandar100@gmail.com>
2023-08-24 16:31:55 +0700

    tambah section just arrived

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
+import JustArrive from '../parts/HomePage/JustArrive'
 
 export default function HomePage() {
   return (
     <div>
         <Header />
         <Hero />
+        <JustArrive />
     </div>
   )
 }
